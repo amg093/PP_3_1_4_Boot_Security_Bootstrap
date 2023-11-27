0b7ffa410d15341b400ef103a364563e53a8691a
Magomed Abasov <sebebsiz93@gmail.com>
2023-11-28 00:22:09 +0300

    Delete FetchType.EAGER, using Transactional

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -27,7 +27,7 @@ public class User implements UserDetails {
 
     private String email;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
